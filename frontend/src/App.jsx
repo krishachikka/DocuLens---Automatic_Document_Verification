83759d31f17a523956780b47752aaeb664c1056f
Yash Chavan <yashchavan4628@gmail.com>
2024-10-01 11:36:41 +0530

    project initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signin from './pages/Signin'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+
+
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
