414b9d80e680f8a63ae547427e0a5fb3eb8c139e
Anjali Gupta <152984851+anjali76Codes@users.noreply.github.com>
2024-10-01 12:40:25 +0530

    Uploaded documents into firebase done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Signin from './pages/Signin'
-import Home from './pages/Home'
-import Signup from './pages/Signup'
-
-
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import DocumentUpload from './pages/DocumentUpload';  // Import DocumentUpload component
 
 function App() {
   return (
@@ -13,12 +11,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path='/signin' element={<Signin />} />
-          <Route path='/signup' element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/upload" element={<DocumentUpload />} />  {/* Add the DocumentUpload route */}
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
